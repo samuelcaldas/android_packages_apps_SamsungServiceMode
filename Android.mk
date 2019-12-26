4f10ca25740c3b32f3e268862185eac9975a4f5b
Samuel Caldas <samuel.caldas@gmail.com>
2019-12-26 12:43:16 -0300

    Changes to be committed: 	modified:   Android.mk 		+#LOCAL_SDK_VERSION := current 		+LOCAL_PRIVATE_PLATFORM_APIS := true
    Change-Id: I1a56d2dcac02e0c73da8b507bf142992d4e35e16

--- Android.mk
+++ Android.mk
@@ -10,7 +10,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := SamsungServiceMode
 LOCAL_JAVA_LIBRARIES := telephony-common
 LOCAL_CERTIFICATE := platform
-LOCAL_SDK_VERSION := current
+#LOCAL_SDK_VERSION := current
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 include $(BUILD_PACKAGE)
 
